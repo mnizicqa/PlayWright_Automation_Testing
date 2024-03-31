df7bff5887c01126a79113505fd670813cf3aaf3
Mario Nizic <marionizic@msn.com>
2024-03-31 15:51:49 +0200

    Added new files regarding Playwright API testing

--- tests/PlaywrightApiTesting.js
+++ tests/PlaywrightApiTesting.js
@@ -1,88 +1,42 @@
 const { test, expect, request } = require("@playwright/test");
+const { APIUtilities } = require("./utilities/APIUtilities");
 const loginPayload = {
   userEmail: "mariotest@gmail.com",
   userPassword: "Qatester1309",
 };
-let loginToken;
+const orderPayload = {
+  orders: [
+    {
+      country: "Croatia",
+      productOrderedId: "6581ca979fd99c85e8ee7faf",
+    },
+  ],
+};
+let createOrder;
+let i;
 
 test.beforeAll(async () => {
   const apiContext = await request.newContext();
-  const loginResponse = await apiContext.post(
-    "https://rahulshettyacademy.com/api/ecom/auth/login",
-    { data: loginPayload }
-  );
-  expect(loginResponse.ok()).toBeTruthy();
-  const loginResponseJson = await loginResponse.json();
-  loginToken = loginResponseJson.token;
-  console.log(loginToken);
+  const apiUtils = new APIUtilities(apiContext, loginPayload);
+  createOrder = await apiUtils.createOrder(orderPayload);
 });
 
-test("Client App Successful Login", async ({ page }) => {
+test("Place the Order Via Api", async ({ page }) => {
   page.addInitScript((value) => {
     window.localStorage.setItem("token", value);
-  }, loginToken);
-  //   await page.goto("https://rahulshettyacademy.com/client");
-  //   await page.locator("#userEmail").fill(email);
-  //   await page.locator("#userPassword").fill("Qatester1309");
-  //   await page.locator("[value='Login']").click();
-  //   await products.first().waitFor();
+  }, createOrder.loginToken);
   await page.goto("https://rahulshettyacademy.com/client");
-  const products = page.locator(".card-body");
-  const productName = "ADIDAS ORIGINAL";
-  const email = "mariotest@gmail.com";
-  const titles = await products.allTextContents();
-  console.log(titles);
-  await expect(page.locator(".m-2.blink_me")).toBeVisible();
-  const count = await products.count();
-  let i;
-
-  for (i = 0; i < count; i++) {
-    if ((await products.nth(i).locator("b").textContent()) === productName) {
-      //add to cart
-      await products.nth(i).locator("text= Add To Cart").click();
-      break;
-    }
-  }
-
-  await page.locator("[routerlink*='cart']").click();
-  await expect(page.locator("h3:has-text('ADIDAS ORIGINAL')")).toBeVisible();
-
-  await page.locator("text=Checkout").click();
-  await page.locator("[placeholder='Select Country']").pressSequentially("bo");
-  const dropdown = page.locator(".ta-results");
-  await dropdown.waitFor();
-  const optionsCount = await dropdown.locator("button").count();
-
-  for (i = 0; i < optionsCount; i++) {
-    const text = await dropdown.locator("button").nth(i).textContent();
-    if (text.trim() == "Bosnia and Herzegowina") {
-      await dropdown.locator("button").nth(i).click();
-      break;
-    }
-  }
-  await expect(page.locator(".user__name label[type='text']")).toHaveText(
-    email
-  );
-  await page.locator(".action__submit").click();
-  await expect(page.locator(".hero-primary")).toHaveText(
-    " Thankyou for the order. "
-  );
-  const orderId = await page
-    .locator(".em-spacer-1 .ng-star-inserted")
-    .textContent();
-  console.log(orderId);
-  await page.locator("button[routerlink *= 'orders']").click();
+  await page.locator("button[routerlink *= 'myorders']").click();
   await expect(page.locator("h1.ng-star-inserted")).toHaveText("Your Orders");
   await page.locator("tbody").waitFor();
   const rows = await page.locator("tbody tr");
   for (i = 0; i < (await rows.count()); i++) {
     const rowOrderId = await rows.nth(i).locator("th").textContent();
-    if (orderId.includes(rowOrderId)) {
+    if (createOrder.orderId.includes(rowOrderId)) {
       await rows.nth(i).locator("text=View").click();
       break;
     }
   }
-
   const myOrderDescription = await page.locator(".col-text").textContent();
-  expect(orderId.includes(myOrderDescription)).toBeTruthy();
+  expect(createOrder.orderId.includes(myOrderDescription)).toBeTruthy();
 });
