41f43533189496a978f3483cee5343f14c3abd10
Mario Nizic <marionizic@msn.com>
2024-04-04 13:35:43 +0200

    Added test to validate upload and download file feature in Playwright

--- tests/PlaywrightUploadDownloadExcel.js
+++ tests/PlaywrightUploadDownloadExcel.js
@@ -1,4 +1,5 @@
 const ExcelJs = require("exceljs");
+const { test, expect } = require("@playwright/test");
 
 async function writeExcelTest(searchText, replaceText, change, filePath) {
   const workbook = new ExcelJs.Workbook();
@@ -26,9 +27,32 @@ async function readExcelTest(worksheet, searchText) {
   return output;
 }
 
-writeExcelTest(
-  "Mango",
-  "350",
-  { rowChange: 0, colChange: 2 },
-  "D:/excelDownloadTest.xlsx"
-);
+// writeExcelTest(
+//   "Mango",
+//   "350",
+//   { rowChange: 0, colChange: 2 },
+//   "D:/excelDownloadTest.xlsx"
+// );
+
+test("File Download and Upload Validation", async ({ page }) => {
+  const desiredFruit = "Mango";
+  const desiredPrice = "350";
+  await page.goto("https://rahulshettyacademy.com/upload-download-test/");
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByRole("button", { name: "Download" }).click();
+  const downloadedFile = await downloadPromise;
+  await downloadedFile.saveAs("C:/Users/mario/Downloads/download.xlsx");
+  writeExcelTest(
+    desiredFruit,
+    desiredPrice,
+    { rowChange: 0, colChange: 2 },
+    "C:/Users/mario/Downloads/download.xlsx"
+  );
+  await page.locator("#fileinput").click();
+  await page
+    .locator("#fileinput")
+    .setInputFiles("C:/Users/mario/Downloads/download.xlsx");
+  const textToLocate = page.getByText(desiredFruit);
+  const desiredRow = page.locator("#row-0").filter({ has: textToLocate });
+  await expect(desiredRow.locator("#cell-4-undefined")).toContainText("350");
+});
